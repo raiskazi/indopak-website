4c07a1d10e9d9b2ab7e617a28cd05bd2f9acc3af
raiskazi <56422384+raiskazi@users.noreply.github.com>
2021-01-05 22:14:31 -0600

    Cafe Start

--- main.py
+++ main.py
@@ -1,16 +1,12 @@
-from flask import Flask, render_template, url_for
-app = Flask(__name__)
+from flask import Flask, render_template, url_for, request
+from markupsafe import escape 
 
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template('index.html')
-
-@app.route("/cafe")
-def cafe():
-    return render_template('cafe.html')
-
-
+    
 @app.route("/butcher")
 def butcher():
     return render_template('butcher.html')
@@ -22,3 +18,10 @@ def banquet():
 @app.route("/about")
 def about():
     return render_template('about.html')
+
+@app.route('/cafe/')
+@app.route('/cafe/<location>')
+def cafe(location=None):
+    if location is None or (location != "plano" and location !="richardson" and location !="arlington" and location !="lewisville" and location !="carrollton"):
+        return render_template("cafe/cafe.html")
+    return render_template("cafe/" + location + ".html")
