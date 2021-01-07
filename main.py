0d2aaa8aa0f1e5b9b3ad5570eee1c36abf69270c
Tahmid Imran <tahmidimran1@gmail.com>
2021-01-06 21:45:23 -0600

    download mdb

--- main.py
+++ main.py
@@ -1,27 +1,36 @@
 from flask import Flask, render_template, url_for, request
-from markupsafe import escape 
-
+from datetime import datetime
 app = Flask(__name__)
+
+
+@app.context_processor
+def inject_now():
+    return {'now': datetime.utcnow()}
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
-    
+
+
 @app.route("/butcher")
 def butcher():
     return render_template('butcher.html')
+
 
 @app.route("/banquet")
 def banquet():
     return render_template('banquet.html')
 
+
 @app.route("/about")
 def about():
     return render_template('about.html')
 
+
 @app.route('/cafe/')
 @app.route('/cafe/<location>')
 def cafe(location=None):
-    if location is None or (location != "plano" and location !="richardson" and location !="arlington" and location !="lewisville" and location !="carrollton"):
+    if location is None or (location != "plano" and location != "richardson" and location != "arlington" and location != "lewisville" and location != "carrollton"):
         return render_template("cafe/cafe.html")
     return render_template("cafe/" + location + ".html")
