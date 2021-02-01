e6c7425dcc01cf19ad62642bdc2f8d6f5ad11eb9
Tahmid Imran <tahmidimran1@gmail.com>
2021-01-31 18:57:25 -0600

    changes for aws

--- application.py
+++ application.py
@@ -1,15 +1,15 @@
 from flask import Flask, render_template, url_for, request
 from datetime import datetime
 import json
-app = Flask(__name__)
+application = Flask(__name__)
 
 
-@app.context_processor
+@application.context_processor
 def inject_now():
     return {'now': datetime.utcnow()}
 
 
-@app.context_processor
+@application.context_processor
 def utility_processor():
     def format_price(amount, currency=u'$'):
         if amount is None:
@@ -18,31 +18,38 @@ def utility_processor():
     return dict(format_price=format_price)
 
 
-@app.route("/")
+@application.route("/")
 def home():
     return render_template('index.html')
 
 
-@app.route("/butcher")
+@application.route("/butcher")
 def butcher():
     return render_template('butcher.html')
 
 
-@app.route("/banquet")
+@application.route("/banquet")
 def banquet():
     return render_template('banquet.html')
 
 
-@app.route("/about")
+@application.route("/about")
 def about():
     return render_template('about.html')
 
 
-@app.route('/cafe/')
-@app.route('/cafe/<location>')
+@application.route('/cafe/')
+@application.route('/cafe/<location>')
 def cafe(location=None):
     if location is None or (location != "plano" and location != "richardson" and location != "arlington" and location != "lewisville" and location != "carrollton" and location != "irving" and location != "murphy"):
         return render_template("cafe/cafe.html")
     with open('cafe.json') as f:
         data = json.load(f)
     return render_template("cafe/" + location + ".html", data=data[location])
+
+
+if __name__ == "__main__":
+    # Setting debug to True enables debug output. This line should be
+    # removed before deploying a production application.
+    application.debug = True
+    application.run()
